5914beb5f07a66628a009d83df4730c83dc2f4e6
Moyang He <iammoyang@gmailcom>
2021-09-06 11:29:00 -0700

    Updates

---
+++ precache-manifest.cf5b9895b4c4179b23df6203d3b05b7b.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59599eed5389f2d53a86d180d3b0dcc4",
+    "url": "/uscis-case-statistics/index.html"
+  },
+  {
+    "revision": "c746099ba43b4feb9ff5",
+    "url": "/uscis-case-statistics/static/js/2.99382f04.chunk.js"
+  },
+  {
+    "revision": "fd8cd4627438764e51f3ee3f549a67df",
+    "url": "/uscis-case-statistics/static/js/2.99382f04.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d394e0fbd70e4860e37",
+    "url": "/uscis-case-statistics/static/js/3.fb12f3db.chunk.js"
+  },
+  {
+    "revision": "26e0c9c35866e7fa8be7",
+    "url": "/uscis-case-statistics/static/js/4.0126472a.chunk.js"
+  },
+  {
+    "revision": "80f86ef322f6ec725deb",
+    "url": "/uscis-case-statistics/static/js/main.e3e2b93a.chunk.js"
+  },
+  {
+    "revision": "2a14155b74d2536a61bb",
+    "url": "/uscis-case-statistics/static/js/runtime-main.7a69b156.js"
+  },
+  {
+    "revision": "e778d75a8e35422f3c7aaf943ada6e5c",
+    "url": "/uscis-case-statistics/static/media/case_number_format.e778d75a.png"
+  },
+  {
+    "revision": "84e04efe993c3656d06b9df04b20c2e2",
+    "url": "/uscis-case-statistics/static/media/donation_wechat.84e04efe.jpg"
+  },
+  {
+    "revision": "b7e75b32b069f3168a2adf534fb77758",
+    "url": "/uscis-case-statistics/static/media/monetization.b7e75b32.jpg"
+  },
+  {
+    "revision": "76bf9850aed2b35898dfd71034ae6960",
+    "url": "/uscis-case-statistics/static/media/venmo.76bf9850.png"
+  },
+  {
+    "revision": "b70b83ac95c8001b59cc7bc8ba1989f7",
+    "url": "/uscis-case-statistics/static/media/wechat_group_qr.b70b83ac.jpg"
+  },
+  {
+    "revision": "6a2dd925042336ac382873d0b0a12e7c",
+    "url": "/uscis-case-statistics/static/media/wechat_qr.6a2dd925.jpg"
+  }
+]);
