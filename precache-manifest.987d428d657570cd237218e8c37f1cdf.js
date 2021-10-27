61ab24af81a9d3bae75ab67964b5d7d916f61871
Moyang He <iammoyang@gmailcom>
2021-10-27 10:26:38 -0700

    Updates

---
+++ precache-manifest.987d428d657570cd237218e8c37f1cdf.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0ba8b100c9ec983f4430e0ce442ccc5",
+    "url": "/uscis-case-statistics/index.html"
+  },
+  {
+    "revision": "cd3aa3fec8abe56c0f92",
+    "url": "/uscis-case-statistics/static/js/2.6478feae.chunk.js"
+  },
+  {
+    "revision": "fd8cd4627438764e51f3ee3f549a67df",
+    "url": "/uscis-case-statistics/static/js/2.6478feae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d75933389c9ce1f73f17",
+    "url": "/uscis-case-statistics/static/js/3.8c5a9883.chunk.js"
+  },
+  {
+    "revision": "d91115461c4524ca7fc1",
+    "url": "/uscis-case-statistics/static/js/4.6edc7f3a.chunk.js"
+  },
+  {
+    "revision": "0f8f00087f9928a6a7c7",
+    "url": "/uscis-case-statistics/static/js/5.35311b2f.chunk.js"
+  },
+  {
+    "revision": "3828165f7143e5399a93",
+    "url": "/uscis-case-statistics/static/js/main.0b92ec9c.chunk.js"
+  },
+  {
+    "revision": "5056b73d2cf9a94a821d",
+    "url": "/uscis-case-statistics/static/js/runtime-main.e1a69c5c.js"
+  },
+  {
+    "revision": "e778d75a8e35422f3c7aaf943ada6e5c",
+    "url": "/uscis-case-statistics/static/media/case_number_format.e778d75a.png"
+  },
+  {
+    "revision": "84e04efe993c3656d06b9df04b20c2e2",
+    "url": "/uscis-case-statistics/static/media/donation_wechat.84e04efe.jpg"
+  },
+  {
+    "revision": "b7e75b32b069f3168a2adf534fb77758",
+    "url": "/uscis-case-statistics/static/media/monetization.b7e75b32.jpg"
+  },
+  {
+    "revision": "76bf9850aed2b35898dfd71034ae6960",
+    "url": "/uscis-case-statistics/static/media/venmo.76bf9850.png"
+  },
+  {
+    "revision": "b70b83ac95c8001b59cc7bc8ba1989f7",
+    "url": "/uscis-case-statistics/static/media/wechat_group_qr.b70b83ac.jpg"
+  },
+  {
+    "revision": "6a2dd925042336ac382873d0b0a12e7c",
+    "url": "/uscis-case-statistics/static/media/wechat_qr.6a2dd925.jpg"
+  }
+]);
